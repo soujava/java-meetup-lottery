1a5f12238e2b99206850337c742658fa325cb3ed
Estevão Hilst <xtevan@gmail.com>
2018-03-24 18:28:22 -0300

    Adjustments in layout, added Init Draw and Draw Next buttons, code refactored

--- src/main/java/com/meetupjavasaopaulo/lottery/HomeController.java
+++ src/main/java/com/meetupjavasaopaulo/lottery/HomeController.java
@@ -4,16 +4,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.util.ArrayUtils;
 
 import javax.servlet.http.HttpSession;
 
@@ -23,21 +26,22 @@ public class HomeController {
     @Value("classpath:meetup.xls")
     private Resource res;
 
+    @Autowired
+    private HttpSession session;
+
     private static final String GUESTS = "guests";
     private static final String WINNERS = "winners";
 
+
+
+
     @GetMapping("/")
-    public ModelAndView home(HttpSession session) {
+    public ModelAndView home() {
 
-        List<String> guests = (ArrayList<String>)session.getAttribute(GUESTS);
-        Stack<String> winners = (Stack<String>)session.getAttribute(WINNERS);
+        List<String> guests = getGuests();
+        Stack<String> winners = getWinners();
 
-        if(guests == null){
-            guests = new ArrayList<>();
-            winners = new Stack<>();
-        }
-
-        String winner = winners.empty()? "": winners.peek();
+        String winner = winners.empty()? "Draw has not started": winners.peek();
         ModelAndView mv = new ModelAndView("home");
         mv.addObject(GUESTS, guests);
         mv.addObject("size", guests.size());
@@ -49,36 +53,61 @@ public class HomeController {
     }
 
     @GetMapping("/init")
-    public ModelAndView startDraw(HttpSession session) {
-        loadFile(session);
-        session.setAttribute(WINNERS,new Stack<String>());
+    public ModelAndView startDraw() {
+        loadFile();
 
-        return home(session);
+        return home();
     }
 
     @GetMapping("/draw")
-    public ModelAndView drawNext(HttpSession session) {
-        List<String> guests = (ArrayList<String>)session.getAttribute(GUESTS);
-        Stack<String> winners = (Stack<String>)session.getAttribute(WINNERS);
+    public ModelAndView drawNext() {
+        List<String> guests = getGuests();
+        Stack<String> winners = getWinners();
         int winner = RandomUtil.getRandomNumberInRange(0, guests.size());
+        System.out.println("Winner index:"+ winner);
         winners.push(guests.remove(winner));
 
-        return home(session);
+        return home();
     }
 
-    private void loadFile(HttpSession session) {
+    private void loadFile() {
 
-        System.out.println("All files:");
+        System.out.println("Load guests file");
         try (Stream<String> stream = Files.lines(Paths.get(res.getURI()))) {
-            List<String> guests = new ArrayList<>();
-            guests = stream.collect(Collectors.toList());
+            List<String> guests = stream.collect(Collectors.toList());
 
             System.out.println("Stream size: " + guests.size());
 
-            session.setAttribute(GUESTS,guests);
+            if(guests.get(0).contains("Name\tUser ID")) //Remove file header
+                guests.remove(0);
+
+            setGuests(guests);
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
+    }
+
+    private List<String> getGuests(){
+
+        if(session.getAttribute(GUESTS) == null)
+            setGuests(new ArrayList<>());
+
+        return  (ArrayList<String>)session.getAttribute(GUESTS);
+    }
+
+    private void setGuests(List<String> guests){
+        session.setAttribute(GUESTS,guests);
+    }
+
+    private Stack<String> getWinners() {
+        if(session.getAttribute(WINNERS) == null)
+            setWinners(new Stack<>());
+
+        return (Stack<String>)session.getAttribute(WINNERS);
+    }
+
+    private void setWinners(Stack<String> winners) {
+        session.setAttribute(WINNERS, winners);
     }
 
 }
